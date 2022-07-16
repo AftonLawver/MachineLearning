7368696f0b59688be6ee04142248404f80bb1ec1
afton21 <lawverap25@gmail.com>
2022-07-16 06:32:19 -0500

    Started working with pandas.

--- pandasDemo.py
+++ pandasDemo.py
@@ -1,6 +1,12 @@
-import pandas as pd
+# import pandas as pd
+#
+# url = "datasets/automobiles.data"
+# df = pd.read_csv(url)
+# print(df.head())
 
-url = "datasets/automobiles.data"
-df = pd.read_csv(url)
-print(df.head())
+my_list1 = ['afton', 'alliyah']
 
+my_list2 = [2, 1]
+
+new_list = list(zip(my_list2, my_list1))
+print(new_list)
